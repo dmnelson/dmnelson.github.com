57ffa7b9702d3415b5fe15dcd252280023da3ec2
David Michael Nelson <davidmichael.nelson@gmail.com>
2011-10-08 23:28:27 -0300

    Baseline implementation

--- application.rb
+++ application.rb
@@ -9,10 +9,3 @@ before do
     response.headers['Cache-Control'] = 'public, max-age=31557600' # 1 year
 end
 
-get '/' do
-    File.read('_site/index.html')
-end
-
-post '/' do
-    File.read('_site/index.html')
-end
